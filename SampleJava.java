f9450761c1579de1593428cf50093ab6ddf7e574
bpalaniswamy-aie <palaniswamybalaji64@gmail.com>
2021-09-14 12:22:26 +0530

    Sample class created

---
+++ SampleJava.java
@@ -0,0 +1,10 @@
+public class SampleJava{
+	public static int add(int a, int b){
+		return a+b;
+	}
+	
+	public static void main(String[] Args){
+		int a=30, b=10;
+		System.out.println(SampleJava.add(a,b));
+	}
+}
