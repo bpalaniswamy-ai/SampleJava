25b4b67e21afceb85f70c9b2b643f409505eaa69
bpalaniswamy-aie <palaniswamybalaji64@gmail.com>
2021-09-14 12:31:36 +0530

    Subtraction function added

--- SampleJava.java
+++ SampleJava.java
@@ -3,8 +3,13 @@ public class SampleJava{
 		return a+b;
 	}
 	
+	public static int sub(int a, int b){
+		return a-b;
+	}
+	
 	public static void main(String[] Args){
 		int a=30, b=10;
 		System.out.println(SampleJava.add(a,b));
+		System.out.println(SampleJava.sub(a,b));
 	}
 }
